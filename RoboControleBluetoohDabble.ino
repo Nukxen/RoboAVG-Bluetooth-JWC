8453c8d84efb85902e963a200b40c2f9127172f7
Samael <157515469+Nukxen@users.noreply.github.com>
2025-01-22 10:19:57 -0300

    Update RoboControleBluetoohDabble.ino

--- RoboControleBluetoohDabble.ino
+++ RoboControleBluetoohDabble.ino
@@ -1,6 +1,7 @@
-//Modulo Bluetooth - Arduino 14 está funcionando
-
-#define CUSTOM_SETTINGS
+/*
+Aqui é onde fazemos as definições dos pinos e variaveis usadas no codigo
+*/
+#define CUSTOM_SETTINGS 
 #define INCLUDE_GAMEPAD_MODULE
 #define echoPin 0
 #define trigPin 1
@@ -14,26 +15,9 @@ int distance;
 
 int speed = 100,PWMspeed;
 
-void setup() {
-  Serial.begin(250000);      // make sure your Serial Monitor is also set at this baud rate.
-  Dabble.begin(9600);
-  pinMode(MDn,OUTPUT);
-  pinMode(MDp,OUTPUT);
-  pinMode(MEp,OUTPUT);
-  pinMode(MEn,OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-
-  PWMspeed = map(speed,0,100,0,1023);
-}
-
-void loop() {
-  Dabble.processInput();
-  Move();
-}
-
 void Move(){
- 
+  //Move é a função de movimento, ela usa o GamePad que é um obj vindo do Dabble pra realizar testes nas entradas no aplicativo,
+  //fazendo com que ao controlar ele remotamente ele saiba qual tecla esta sendo pressionada
   if (GamePad.isUpPressed())
   {
     digitalWrite(MDp,LOW);
@@ -76,6 +60,8 @@ void Move(){
 }
 
 void echo(){
+  //Echo é a função ligada ao HCSR-04, o qual não é utilizada nesse progama!
+
   //Emitir pulso 10us no trigger
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -89,14 +75,29 @@ void echo(){
   // Calcula a distância em centímetros
   distance = duration * 0.034 / 2;
 
+  //Imprime a diancia no serial
   Serial.print("Distancia: ");
   Serial.print(distance);
   Serial.println(" cm");
-
-  if(distance < 15){
-    digitalWrite(MDp,LOW);
-    digitalWrite(MEp,LOW);
-    digitalWrite(MEn,LOW);
-    digitalWrite(MDn,LOW);
-  }
 }
+
+void setup() {
+  // Aqui no setup, temos a parte do codigo que será executado uma unica vez, e ela é responsavel por inicializar certos parametros do codigo pra que o progama os acesse conforme a necessidade
+  Serial.begin(250000);      
+  Dabble.begin(9600);
+  pinMode(MDn,OUTPUT);
+  pinMode(MDp,OUTPUT);
+  pinMode(MEp,OUTPUT);
+  pinMode(MEn,OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  PWMspeed = map(speed,0,100,0,1023);
+}
+
+void loop() {
+  //Loop é a parte do codigo que ficara em execução constante
+  Dabble.processInput();
+  Move();
+}
+
